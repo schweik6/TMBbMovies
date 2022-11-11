161a996bbcb8d280a3a28add454181cda3a1173b
yicheng <1415252183@qq.com>
2022-11-11 22:35:41 +0800

    Initial Commit

---
+++ TMBbMovies/TMBbMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMBbMoviesApp.swift
+//  TMBbMovies
+//
+//  Created by xmly on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct TMBbMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
